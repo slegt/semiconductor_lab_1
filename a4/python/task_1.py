14d74504a0b0b7051856fb349c06311090b0a31d
Simon Legtenborg <simon.legtenborg@singular-it.de>
2025-08-12 15:59:20 +0200

    feat: A4 add write task 1 and task 2

--- a4/python/task_1.py
+++ a4/python/task_1.py
@@ -8,45 +8,45 @@ import pathlib
 
 file_directory = pathlib.Path(__file__).parent.resolve()
 
-P_SI = {
-    "T": np.array([300, 300, 300, 300]),
-    "I": np.array([0.01, 0.01, 0.01, 0.01]),
-    "U1": np.array([-5.69, -5.63, 5.86, 5.87]) * 10**-4,
-    "U2": np.array([-3.92, -3.92, 3.99, 4.02]) * 10**-4,
-    "U3": np.array([1.84, -1.77, -1.72, 1.87]) * 10**-4,
-    "U4": np.array([1.90, -1.72, -1.78, 1.81]) * 10**-4,
-    "d": 500e-6,
+data = {
+    "P_SI": {
+        "T": np.array([300, 300, 300, 300]),
+        "I": np.array([0.01, 0.01, 0.01, 0.01]),
+        "U1": np.array([-5.69, -5.63, 5.86, 5.87]) * 10**-4,
+        "U2": np.array([-3.92, -3.92, 3.99, 4.02]) * 10**-4,
+        "U3": np.array([1.84, -1.77, -1.72, 1.87]) * 10**-4,
+        "U4": np.array([1.90, -1.72, -1.78, 1.81]) * 10**-4,
+        "d": 500e-6,
+    },
+    "ZnO": {
+        "T": np.array([300, 300, 300, 300]),
+        "I": np.array([4.3, 4.3, 4.3, 4.3]) * 10e-4,
+        "U1": np.array([-3.09, -3.09, 3.09, 3.09]) * 10e-1,
+        "U2": np.array([-3.25, -3.25, 3.25, 3.25]) * 10e-1,
+        "U3": np.array([-1.59, 1.6, 1.59, -1.6]) * 10e-2,
+        "U4": np.array([-1.76, 1.43, 1.76, -1.43]) * 10e-2,
+        "d": 1e-6,
+    },
+    "ZTO": {
+        "T": np.array([300, 300, 300, 300]),
+        "I": np.array([8.5, 8.5, 8.5, 8.5]) * 10e-3,
+        "U1": np.array([-2.75, -2.75, 2.75, 2.74]) * 10e-1,
+        "U2": np.array([-2.87, -2.87, 2.87, 2.87]) * 10e-1,
+        "U3": np.array([-1.26, 1.25, 1.23, -1.29]) * 10e-2,
+        "U4": np.array([-1.33, 1.18, 1.3, -1.23]) * 10e-2,
+        "d": 1.3e-6,
+    },
+    "CuI": {
+        "T": np.array([300, 300, 300, 300]),
+        "I": np.array([0.03, 0.03, 0.03, 0.03]),
+        "U1": np.array([-1.7, -1.68, 1.75, 1.75]) * 10e-3,
+        "U2": np.array([-1.19, -1.19, 1.17, 1.18]) * 10e-3,
+        "U3": np.array([5.27, -5.52, -5.18, 5.45]) * 10e-4,
+        "U4": np.array([5.45, -5.34, -5.36, 5.28]) * 10e-4,
+        "d": 330e-9,
+    },
 }
 
-CuI = {
-    "T": np.array([300, 300, 300, 300]),
-    "I": np.array([0.03, 0.03, 0.03, 0.03]),
-    "U1": np.array([-1.7, -1.68, 1.75, 1.75]) * 10e-3,
-    "U2": np.array([-1.19, -1.19, 1.17, 1.18]) * 10e-3,
-    "U3": np.array([5.27, -5.52, -5.18, 5.45]) * 10e-4,
-    "U4": np.array([5.45, -5.34, -5.36, 5.28]) * 10e-4,
-    "d": 330e-9,
-}
-
-ZTO = {
-    "T": np.array([300, 300, 300, 300]),
-    "I": np.array([8.5, 8.5, 8.5, 8.5]) * 10e-3,
-    "U1": np.array([-2.75, -2.75, 2.75, 2.74]) * 10e-1,
-    "U2": np.array([-2.87, -2.87, 2.87, 2.87]) * 10e-1,
-    "U3": np.array([-1.26, 1.25, 1.23, -1.29]) * 10e-2,
-    "U4": np.array([-1.33, 1.18, 1.3, -1.23]) * 10e-2,
-    "d": 1.3e-6,
-}
-
-ZnO = {
-    "T": np.array([300, 300, 300, 300]),
-    "I": np.array([4.3, 4.3, 4.3, 4.3]) * 10e-4,
-    "U1": np.array([-3.09, -3.09, 3.09, 3.09]) * 10e-1,
-    "U2": np.array([-3.25, -3.25, 3.25, 3.25]) * 10e-1,
-    "U3": np.array([-1.59, 1.6, 1.59, -1.6]) * 10e-2,
-    "U4": np.array([-1.76, 1.43, 1.76, -1.43]) * 10e-2,
-    "d": 1e-6,
-}
 
 B = 0.43
 resistivity_correction = [-1, -1, 1, 1]
@@ -83,7 +83,7 @@ def calculate_resistivities(measurement_dict):
         rhos.append(float(rho))
 
     rhos = [a * b for a, b in zip(rhos, resistivity_correction)]
-    return rhos
+    return np.array(rhos)
 
 
 def calculate_hall_coefficient(measurement_dict):
@@ -97,7 +97,15 @@ def calculate_hall_coefficient(measurement_dict):
         halls.append(hall)
 
     rhos = [a * b for a, b in zip(halls, hall_correction)]
-    return rhos
+    return np.array(rhos)
+
+
+def calculate_hall_concentration(hall_coefficient):
+    return np.abs(1 / e * 1 / hall_coefficient)
+
+
+def calculate_hall_mobility(hall_coefficient, resistivity):
+    return hall_coefficient / resistivity
 
 
 def float_format(x):
@@ -106,50 +114,67 @@ def float_format(x):
 
 
 if __name__ == "__main__":
-    p_si_r = np.array(calculate_resistivities(P_SI))
-    zno_r = np.array(calculate_resistivities(ZnO))
-    zto_r = np.array(calculate_resistivities(ZTO))
-    cui_r = np.array(calculate_resistivities(CuI))
+    resistivities = {key: calculate_resistivities(value) for key, value in data.items()}
+    hall_coefficients = {key: calculate_hall_coefficient(value) for key, value in data.items()}
+    hall_mobility = {key: calculate_hall_mobility(hall_coefficients[key], resistivities[key]) for key in data.keys()}
+    hall_concentration = {key: calculate_hall_concentration(hall_coefficients[key]) for key in data.keys()}
 
-    df = pd.DataFrame(columns=["Property", "P-Si", "ZnO", "ZTO", "CuI"])
+    df = pd.DataFrame(columns=["Property", "p-Si", "ZnO", "ZTO", "CuI"])
+
     for i in range(0, 4):
-        df.loc[i] = [f"$\\rho_{{{i + 1}}}$", p_si_r[i], zno_r[i], zto_r[i], cui_r[i]]
-    df.loc[4] = ["$\\rho_\\text{avg}$", np.mean(p_si_r), np.mean(zno_r), np.mean(zto_r), np.mean(cui_r)]
-    df.loc[5] = ["$\\rho_\\text{std}$", np.std(p_si_r), np.std(zno_r), np.std(zto_r), np.std(cui_r)]
-
-    p_si_h = np.array(calculate_hall_coefficient(P_SI))
-    zno_h = np.array(calculate_hall_coefficient(ZnO))
-    zto_h = np.array(calculate_hall_coefficient(ZTO))
-    cui_h = np.array(calculate_hall_coefficient(CuI))
+        table_resistivities = [resistivities[key][i] for key in data.keys()]
+        df.loc[i] = [f"$\\rho_{{{i + 1}}}$", *table_resistivities]
+    mean_resistivities = {key: np.mean(value) for key, value in resistivities.items()}
+    std_resistivities = {key: np.std(value) for key, value in resistivities.items()}
+    df.loc[4] = ["$\\rho_\\text{avg}$", *mean_resistivities.values()]
+    df.loc[5] = ["$\\rho_\\text{std}$", *std_resistivities.values()]
 
     for i in range(6, 10):
-        df.loc[i] = [f"$R_{{\\mathrm{{H}},{i - 5}}}$", p_si_h[i - 6], zno_h[i - 6], zto_h[i - 6], cui_h[i - 6]]
-    df.loc[10] = ["$R_{\\mathrm{H},\\text{avg}}$", np.mean(p_si_h), np.mean(zno_h), np.mean(zto_h), np.mean(cui_h)]
-    df.loc[11] = ["$R_{\\mathrm{H},\\text{std}}$", np.std(p_si_h), np.std(zno_h), np.std(zto_h), np.std(cui_h)]
-
-    p_si_m = p_si_h / p_si_r
-    zno_m = zno_h / zno_r
-    zto_m = zto_h / zto_r
-    cui_m = cui_h / cui_r
+        table_hall_coefficients = [hall_coefficients[key][i - 6] for key in data.keys()]
+        df.loc[i] = [f"$R_{{\\mathrm{{H}},{i - 5}}}$", *table_hall_coefficients]
+    mean_hall_coefficients = {key: np.mean(value) for key, value in hall_coefficients.items()}
+    std_hall_coefficients = {key: np.std(value) for key, value in hall_coefficients.items()}
+    df.loc[10] = ["$R_{\\mathrm{H},\\text{avg}}$", *mean_hall_coefficients.values()]
+    df.loc[11] = ["$R_{\\mathrm{H},\\text{std}}$", *std_hall_coefficients.values()]
 
     for i in range(12, 16):
-        df.loc[i] = [f"$\\mu_{{{i - 11}}}$", p_si_m[i - 12], zno_m[i - 12], zto_m[i - 12], cui_m[i - 12]]
-    df.loc[16] = ["$\\mu_\\text{avg}$", np.mean(p_si_m), np.mean(zno_m), np.mean(zto_m), np.mean(cui_m)]
-    df.loc[17] = ["$\\mu_\\text{std}$", np.std(p_si_m), np.std(zno_m), np.std(zto_m), np.std(cui_m)]
-
-    p_si_n = np.abs(1 / e * 1 /p_si_h)
-    zno_n = np.abs(1 / e * 1 /zno_h)
-    zto_n = np.abs(1 / e * 1 /zto_h)
-    cui_n = np.abs(1 / e * 1 /cui_h)
+        table_hall_concentration = [hall_concentration[key][i - 12] for key in data.keys()]
+        df.loc[i] = [f"$n_{{{i - 11}}}$", *table_hall_concentration]
+    mean_hall_concentration = {key: np.mean(value) for key, value in hall_concentration.items()}
+    std_hall_concentration = {key: np.std(value) for key, value in hall_concentration.items()}
+    df.loc[16] = ["$n_\\text{avg}$", *mean_hall_concentration.values()]
+    df.loc[17] = ["$n_\\text{std}$", *std_hall_concentration.values()]
 
     for i in range(18, 22):
-        df.loc[i] = [f"$n_{{{i - 17}}}$", p_si_n[i - 18], zno_n[i - 18], zto_n[i - 18], cui_n[i - 18]]
-    df.loc[22] = ["$n_\\text{avg}$", np.mean(p_si_n), np.mean(zno_n), np.mean(zto_n), np.mean(cui_n)]
-    df.loc[23] = ["$n_\\text{std}$", np.std(p_si_n), np.std(zno_n), np.std(zto_n), np.std(cui_n)]
+        table_hall_mobility = [hall_mobility[key][i - 18] for key in data.keys()]
+        df.loc[i] = [f"$\\mu_{{\\mathrm{{H}},{i - 17}}}$", *table_hall_mobility]
+    mean_hall_mobility = {key: np.mean(value) for key, value in hall_mobility.items()}
+    std_hall_mobility = {key: np.std(value) for key, value in hall_mobility.items()}
+    df.loc[22] = ["$\\mu_{\\mathrm{H},\\text{avg}}$", *mean_hall_mobility.values()]
+    df.loc[23] = ["$\\mu_{\\mathrm{H},\\text{std}}$", *std_hall_mobility.values()]
 
+    save_path = file_directory.parent / "plots" / "resistivity_hall.tex"
     df.to_latex(
-        file_directory / ".." / "plots" / "resistivity_hall.tex",
+        buf=str(save_path),
         index=False,
         escape=False,
         float_format=float_format,
     )
+
+    with open(save_path, "r") as file:
+        lines = file.readlines()
+        for index, line_index in enumerate([10,16,22]):
+            lines.insert(line_index + index, "\\midrule\n")
+    with open(save_path, "w") as file:
+        file.writelines(lines)
+
+shorted_df = df.iloc[[4,10,16,22]]
+units = [r"\unit{\ohm\meter}", r"\unit{\meter^3 \per \coulomb}", r"\unit{m^{-3}}", r"\unit{\meter^2 \per \volt \per \second}"]
+units = [f"({unit})" for unit in units]
+shorted_df.loc[:, "Property"] = [text + " " + unit for text, unit in zip(shorted_df["Property"], units)]
+shorted_df.to_latex(
+    buf=str(file_directory.parent / "plots" / "resistivity_hall_short.tex"),
+    index=False,
+    escape=False,
+    float_format=float_format,
+)
--- a4/python/task_2.py
+++ a4/python/task_2.py
@@ -1,13 +1,16 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
 import pathlib
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.constants import k, e
 from scipy.optimize import curve_fit
+import json
 
 latex_column_width = 221.48395  # pt
 pt_to_inch = latex_column_width / 72  # 1 inch = 72 points
-a = pt_to_inch * 0.98 
+a = pt_to_inch * 0.98
 
 mpl.rcParams.update(
     {
@@ -24,9 +27,13 @@ mpl.rcParams.update(
 def linear(x, a, b):
     return a * x + b
 
+def calculate_donor_energy(m):
+    return - 2 * k * m / e * 1000 # in meV
+
 
 # file location
 file = pathlib.Path(__file__).parent.resolve()
+latex_data = dict()
 
 # Read the data from the csv file
 data = pd.read_csv(filepath_or_buffer=file.parent / "data" / "ZnO.txt", sep="\t")
@@ -41,6 +48,8 @@ n_over_t = np.log(n * T_1 ** (-3 / 4))
 
 # Fit the data to a linear model
 m, cov = curve_fit(linear, reciprocal_T[8:], n_over_t[8:])
+latex_data["concentration_m"] = round(m[0], 2)
+latex_data["donor_energy"] = round(calculate_donor_energy(m[0]), 2)
 
 plt.scatter(reciprocal_T * 1000, n_over_t, label="Data", s=8)
 plt.plot(
@@ -62,7 +71,7 @@ plt.ylabel(r"$\ln(nT^{-3/4}) \quad [\ln(\unit{m^{-3} K^{-3 / 4}}]$")
 plt.tight_layout()
 plt.legend()
 plt.savefig(file.parent / "plots" / "task_2_n.pdf")
-plt.show()
+plt.close()
 
 
 T_2 = data["T"].to_numpy()
@@ -73,24 +82,24 @@ mu_log = np.log(mu)
 m1, cov1 = curve_fit(linear, T_2_log[:10], mu_log[:10])
 m2, cov2 = curve_fit(linear, T_2_log[14:21], mu_log[14:21])
 m3, cov3 = curve_fit(linear, T_2_log[23:-8], mu_log[23:-8])
+latex_data["mu_m1"] = round(m1[0], 2)
+latex_data["mu_m2"] = round(m2[0], 2)
+latex_data["mu_m3"] = round(m3[0], 2)
 
 plt.scatter(T_2_log, mu_log, label="Data", s=8)
-plt.plot(T_2_log[:12], linear(T_2_log[:12], *m1), color="black", linestyle="--", 
-         label="Fit")
+plt.plot(T_2_log[:12], linear(T_2_log[:12], *m1), color="black", linestyle="--", label="Fit")
 plt.plot(T_2_log[12:23], linear(T_2_log[12:23], *m2), color="black", linestyle="--")
 plt.plot(T_2_log[23:], linear(T_2_log[23:], *m3), color="black", linestyle="--")
-plt.text(0.2, 0.1, r"$\text{slope:}$" + "\n" + f"{m1[0]:.2f}", 
-         transform=plt.gca().transAxes, ha='center')
-plt.text(0.51, 0.1, r"$\text{slope:}$"+ "\n" + f"{m2[0]:.2f}",
-         transform=plt.gca().transAxes, ha='center')
-plt.text(0.78, 0.1, r"$\text{slope:}$" + "\n" + f"{m3[0]:.2f}",
-            transform=plt.gca().transAxes, ha='center')
+plt.text(0.2, 0.1, r"$\text{slope:}$" + "\n" + f"{m1[0]:.2f}", transform=plt.gca().transAxes, ha="center")
+plt.text(0.51, 0.1, r"$\text{slope:}$" + "\n" + f"{m2[0]:.2f}", transform=plt.gca().transAxes, ha="center")
+plt.text(0.78, 0.1, r"$\text{slope:}$" + "\n" + f"{m3[0]:.2f}", transform=plt.gca().transAxes, ha="center")
 plt.axvspan(T_2_log[12], T_2_log[23], color="black", alpha=0.1, lw=0)
 plt.xlabel(r"$\ln(T) \quad [\ln(K)]$")
 plt.ylabel(r"$\ln(\mu) \quad [\ln(\unit{cm^2/Vs})]$")
 plt.tight_layout()
 plt.legend()
 plt.savefig(file.parent / "plots" / "task_2_mu.pdf")
-plt.show()
+plt.close()
 
-
+with open(file.parent / "plots" / "task_2_data.json", "w") as f:
+    json.dump(latex_data, f, indent=2)
